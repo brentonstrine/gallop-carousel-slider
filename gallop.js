fcc71329cc86b013a0b6b64668e499df5832ca43
Brenton Strine <brenton.a.strine@jpl.nasa.gov>
2014-12-17 14:16:34 -0800

    adding manual advance/retreat controls, cleanup
    adding manual controls for left/right. also a little cleanup.

--- gallop.js
+++ gallop.js
@@ -1,63 +1,75 @@
 $(document).ready(function(){
-    // settings
+// settings
     var autoPlay = true;
 
-    // init vars
+// init vars
     var $gallop = $(".gallop");
     var $slides = $("li");
     var itms = $slides.length;
     var ct = 0;
+    var controls = '';
 
-
-    //create controls
-    var controls = '<div class="autoplay"></div>';
+// create controls
+    // manual left control
+    controls += '<div class="retreat">&laquo;</div>';
+    // autoplay toggle control
+    controls += '<div class="autoplay"></div>';
+    // individual slide picker controls
     for (var i = 0; i < itms; i++) {
         controls += "<div class='picker' data-item='"+i+"'>"+i+"</div>";
     }
+    // manual right control
+    controls += '<div class="advance">&raquo;</div>';
+    // populate page with controls
     $gallop.append("<div class='controls'/>").find(".controls").append(controls);
     var $pickers = $(".picker");
 
-    // slide forward
-    //$(".gallop").on("click", ".advance", function(){
-    //    gallop();
-    //});
+// slide forward
+    $(".gallop").on("click", ".advance", function(){
+        gallop();
+    });
 
-    // slide backwards
-    //$(".gallop").on("click", ".retreat", function(){
-    //    ct = ct -2;
-    //    gallop();
-    //});
+// slide backwards
+    $(".gallop").on("click", ".retreat", function(){
+        ct = ct -2;
+        gallop();
+    });
 
-    // toggle autoplay
+// toggle autoplay
     $(".gallop").on("click", ".autoplay", function(){
         $(this).toggleClass("off");
         autoPlay = (autoPlay) ? false : true;
         autoGallop();
     });
 
-    // slide pick handler
+// slide pick handler
     $(".gallop").on("click", ".picker", function(){
         pickItem($(this).attr("data-item"));
     });
 
-function gallop() {
-    $slides.removeClass("active").eq(ct%itms).addClass("active");
-    $pickers.removeClass("active").eq(ct%itms).addClass("active");
-    ct++;
-}
-function pickItem(a) {
-    $slides.removeClass("active").eq(a).addClass("active");
-    $pickers.removeClass("active").eq(a).addClass("active");
-    ct = ++a;
-}
-function autoGallop(){
-    if(autoPlay){
-        gallop();
-        setTimeout( autoGallop, 3000 );
+// advance slide
+    function gallop() {
+        $slides.removeClass("active").eq(ct%itms).addClass("active");
+        $pickers.removeClass("active").eq(ct%itms).addClass("active");
+        ct++;
     }
-}
 
-autoGallop();
+// go to specific slide
+    function pickItem(a) {
+        $slides.removeClass("active").eq(a).addClass("active");
+        $pickers.removeClass("active").eq(a).addClass("active");
+        ct = ++a;
+    }
 
+// autoplay
+    function autoGallop(){
+        if(autoPlay){
+            gallop();
+            setTimeout( autoGallop, 3000 );
+        }
+    }
+
+// initiate autoplay
+    autoGallop();
 
 });
