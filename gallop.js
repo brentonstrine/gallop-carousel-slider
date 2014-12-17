eecc9bbca19404e7c471cdb1e9cd9f44a8194f3e
Brenton Strine <brenton.a.strine@jpl.nasa.gov>
2014-12-17 14:24:27 -0800

    fixing timeout bug, improving js readability

--- gallop.js
+++ gallop.js
@@ -1,75 +1,74 @@
 $(document).ready(function(){
-// settings
+    // settings
     var autoPlay = true;
+    var autoPlaySpeed = 3000;
 
-// init vars
+    // init vars
     var $gallop = $(".gallop");
     var $slides = $("li");
     var itms = $slides.length;
     var ct = 0;
     var controls = '';
+    var timeout;
 
-// create controls
-    // manual left control
+    // create controls
     controls += '<div class="retreat">&laquo;</div>';
-    // autoplay toggle control
     controls += '<div class="autoplay"></div>';
-    // individual slide picker controls
     for (var i = 0; i < itms; i++) {
         controls += "<div class='picker' data-item='"+i+"'>"+i+"</div>";
     }
-    // manual right control
     controls += '<div class="advance">&raquo;</div>';
-    // populate page with controls
     $gallop.append("<div class='controls'/>").find(".controls").append(controls);
     var $pickers = $(".picker");
 
-// slide forward
+    // slide forward
     $(".gallop").on("click", ".advance", function(){
         gallop();
     });
 
-// slide backwards
+    // slide backwards
     $(".gallop").on("click", ".retreat", function(){
         ct = ct -2;
         gallop();
     });
 
-// toggle autoplay
+    // toggle autoplay
     $(".gallop").on("click", ".autoplay", function(){
         $(this).toggleClass("off");
         autoPlay = (autoPlay) ? false : true;
         autoGallop();
     });
 
-// slide pick handler
+    // slide pick handler
     $(".gallop").on("click", ".picker", function(){
         pickItem($(this).attr("data-item"));
     });
 
-// advance slide
+    // advance slide
     function gallop() {
         $slides.removeClass("active").eq(ct%itms).addClass("active");
         $pickers.removeClass("active").eq(ct%itms).addClass("active");
         ct++;
     }
 
-// go to specific slide
+    // go to specific slide
     function pickItem(a) {
         $slides.removeClass("active").eq(a).addClass("active");
         $pickers.removeClass("active").eq(a).addClass("active");
         ct = ++a;
     }
 
-// autoplay
+    // autoplay
     function autoGallop(){
         if(autoPlay){
             gallop();
-            setTimeout( autoGallop, 3000 );
+            timeout = setTimeout( autoGallop, autoPlaySpeed );
+        } else {
+            clearTimeout(timeout);
         }
     }
 
-// initiate autoplay
+    // initiate autoplay
     autoGallop();
 
 });
